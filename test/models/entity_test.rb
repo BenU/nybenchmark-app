02d3596c87c2a9213f377e6a0a2bc1f8e92b87a0
Ben Unger <benjamin.d.unger@gmail.com>
2025-12-16 08:48:46 -0300

    Add core model tests and realistic fixtures

--- test/models/entity_test.rb
+++ test/models/entity_test.rb
@@ -3,7 +3,27 @@
 require "test_helper"
 
 class EntityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with valid attributes" do
+    entity = entities(:one) # Uses fixture
+    assert entity.valid?
+  end
+
+  test "should require a name" do
+    entity = Entity.new(slug: "test")
+    assert_not entity.valid?
+    assert_includes entity.errors[:name], "can't be blank"
+  end
+
+  test "should require a slug" do
+    entity = Entity.new(name: "Test")
+    assert_not entity.valid?
+    assert_includes entity.errors[:slug], "can't be blank"
+  end
+
+  test "slug should be unique" do
+    # Try to create a duplicate of entity :one
+    duplicate = Entity.new(name: "Clone", slug: entities(:one).slug)
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:slug], "has already been taken"
+  end
 end
--- test/models/observation_test.rb
+++ test/models/observation_test.rb
@@ -3,7 +3,29 @@
 require "test_helper"
 
 class ObservationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid with numeric value" do
+    obs = observations(:nyc_expenditures)
+    assert obs.valid?
+  end
+
+  test "valid with text value" do
+    obs = observations(:albany_revenue_text)
+    assert obs.valid?
+  end
+
+  test "invalid without any value" do
+    obs = observations(:nyc_expenditures)
+    obs.value_numeric = nil
+    obs.value_text = nil
+    assert_not obs.valid?
+    # Validating your custom method "value_must_be_present"
+    assert_includes obs.errors[:base], "Either numeric value or text value must be present"
+  end
+
+  test "requires page reference" do
+    obs = observations(:nyc_expenditures)
+    obs.page_reference = nil
+    assert_not obs.valid?
+    assert_includes obs.errors[:page_reference], "can't be blank"
+  end
 end
