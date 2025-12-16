d9c9170ed8a9ee6caefe1c42ee90db3cd2c4f51f
Ben Unger <benjamin.d.unger@gmail.com>
2025-12-15 23:28:45 -0300

    fix: rubocop linting error

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get root_url           # <--- Use the standard root helper
+    get root_url # <--- Use the standard root helper
     assert_response :success
   end
 end
